3c54001e876204421c68520c5d20318d798960d1
Lersis09 <133794400+Lersis09@users.noreply.github.com>
2023-09-27 10:32:39 +0000

    виправлення

--- main.py
+++ main.py
@@ -1,63 +1,73 @@
-import math # підключення бібліотеки
+import math  # підключення бібліотеки
+
+
 def task_integer16():
     """A three-digit number is given. Output the number obtained by 
        permuting the digits tens and ones of the original number (for 
        example, 123 will become 132)."""
     try: # перевірка на помилки
         num = int(input("Enter a three-digit number: "))
-    except: # якщо помилка
+    except ValueError: # якщо помилка
         print("Num must be an INTEGER !!!")
         input("Press enter for exit ...")
     else: # якщо немає помилки
-        num = str(num)
-        a = num[0]
-        b = num[2]
-        c = num[1]
-        print(a + b + c)
-      
+        if num > 99 and num >= 1000:
+             print("Invalid input !!!")
+             input("Press enter to exit ...")
+        else: 
+          a = num // 100 # перша цифра
+          b = num // 10 % 10 # друга цифра
+          c = num % 10 # третя цифра
+          new_num = a * 100 + c * 10 + b # нове число
+          print(new_num)
+
+
+
 def task_33():
     """Calculate mathematical expression"""
     try: # перевірка на помилки
       x = float(input("x = "))
-    except: # якщо помилка
+    except ValueError: # якщо помилка
         print("Wrong number!")
     else: # якщо немає помилки
-        num1 = 2 * math.exp(x+0.5)
-        num2 = math.sqrt(math.fabs(3*x - 2 * math.tan(5* \
-        x - math.radians(43))))
-        x3 = x * x * x
         try:
+          # Для підстраховки виконуємо усі математичні операції у блоці try
+          num1 = 2 * math.exp(x+0.5)
+          num2 = math.sqrt(math.fabs(3*x - 2 * math.tan(5* \
+          x - math.radians(43))))
+          x3 = math.pow(x, 3) # функція для піднесення до степеня
           denum = pow(pow(math.sin(x3), 2), 1.0/3.0)* \
           math.log(math.fabs(x3), 5)
           y = num1 * num2 / denum
-        except:
+        except ValueError:
           print("Error calculations!")
+        except ZeroDivisionError:
+          print("Zero division!")
         else:
           print("y = ", y)
-
-
+          
 def task_boolean16():
-    """Given a positive integer. Check the truth of the statement:"The 
-       given number is an even two-digit number."""
-    try:
+      """Given a positive integer. Check the truth of the statement:"The 
+         given number is an even two-digit number."""
+      try:
         num = int(input("Num = "))
-    except:
+      except ValueError:
         print("Num MUST be an integer!!!")
         input("Press enter to exit ...")
-    else:   
-        if num >= 10 and num <= 99 and num % 2 == 0:
-            print("Num is an even two-digit number: ", num)
-        else:
-            print("Num is not an even two-digit number.")
+      else: 
+          if num >= 10 and num <= 99 and num % 2 == 0:
+               print("Num is an even two-digit number: ", num)
+          else:
+               print("Num is not an even two-digit number.")
 
 def main():
-  """Основна функція для виклику одного із завдань за варіантом"""
+
   task_num = int(input("Enter task num: "))
   if task_num == 1:
     task_integer16()
   elif task_num == 2:
     task_33()
-  elif task_num == 3:
+  elif task_num ==3:
     task_boolean16()
   else:
     print("Wrong task num")
